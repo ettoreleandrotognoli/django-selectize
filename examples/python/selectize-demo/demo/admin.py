69833a8ba3faefa1fa1969525f25ce04af16407d
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-12-02 11:16:16 -0200

    admin

--- examples/python/selectize-demo/demo/admin.py
+++ examples/python/selectize-demo/demo/admin.py
@@ -1,3 +1,40 @@
+from django import forms
 from django.contrib import admin
+from selectize.widgets import RemoteSelectize
+from .models import Choice
+from .models import Question
 
-# Register your models here.
+
+class SelectizeAdminMixin(object):
+    class Media:
+        js = (
+            'selectize.js',
+        )
+
+
+class QuestionAdminForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
+        widgets = {
+
+        }
+
+
+@admin.register(Question)
+class QuestionAdmin(SelectizeAdminMixin, admin.ModelAdmin):
+    form = QuestionAdminForm
+
+
+class ChoiceAdminForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = '__all__'
+        widgets = {
+            'question': RemoteSelectize.make_from_model(Question)
+        }
+
+
+@admin.register(Choice)
+class ChoiceAdmin(SelectizeAdminMixin, admin.ModelAdmin):
+    form = ChoiceAdminForm
